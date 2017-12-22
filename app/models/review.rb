f6c4b6508c72c36b681f1bf4276ea9c6a98c5c23
github username <github email address>
2017-12-22 10:35:33 -0500

    Done.

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,4 +1,18 @@
 class Review
-  
-end
 
+  attr_accessor :customer, :restaurant, :content
+
+  @@all = []
+
+  def initialize(customer, restaurant)
+    @content = nil
+    @customer = customer
+    @restaurant = restaurant
+    @@all << self
+  end
+
+  def self.all
+    @@all
+  end
+
+end
