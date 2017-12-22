5dc2af5366e37fa3185bc86789248dd84db92f43
brianabaker <brianaclairbaker@gmail.com>
2017-12-22 10:32:29 -0500

    Briana Baker done

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -1,8 +1,27 @@
 class Restaurant
   attr_accessor :name
 
+  @@all = []
   def initialize(name)
     @name = name
+    @@all << self
+  end
+
+  def self.all
+    @@all
+  end
+
+  def self.find_by_name(name)
+    self.all.select {|restaurant_instance| restaurant_instance.name == name}
+  end
+
+  def reviews
+    Review.all.select{|review_instance| review_instance.restaurant == self}
+  end
+
+  def customers
+    # reviews = Review.all.select{|review_instance| review_instance.restaurant == self}
+    reviews.map {|review| review.customer}
   end
 
 end
--- tools/console.rb
+++ tools/console.rb
@@ -4,4 +4,24 @@ def reload
   load 'config/environment.rb'
 end
 
+briana = Customer.new("Briana", "Baker")
+briana2= Customer.new("Briana", "Roberts")
+graham = Customer.new("Graham", "Milton")
+graham2 = Customer.new("Graham", "McDillon")
+
+restaurant1 = Restaurant.new("Por Fin")
+restaurant2 = Restaurant.new("Bagel Shop")
+restaurant3 = Restaurant.new("Fish Shop")
+
+#added reviews from users
+#has customer instance
+#has restaurant instance
+
+review1 = briana.add_review(restaurant1, "amazing")
+review2 = briana.add_review(restaurant2, "the absolute worst")
+review3 = graham.add_review(restaurant1, "just okay")
+review4 = graham.add_review(restaurant2, "OMG SO DELICIOUS")
+review5 = graham2.add_review(restaurant3, "I think this also works")
+
+
 Pry.start
