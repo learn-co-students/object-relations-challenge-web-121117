529095e92d7d726eb645b6686b3c904f37717a1a
olegchursin <olegchursin@gmail.com>
2017-12-22 10:37:19 -0500

    Done.

--- config/environment.rb
+++ config/environment.rb
@@ -1,3 +1,24 @@
 require 'bundler/setup'
+require 'pry'
 Bundler.require
 require_all 'app'
+
+Customer.new("Alan", "Kay")
+Customer.new("John", "Doe")
+Customer.new("Anita", "Borg")
+
+5.times do |i|
+  Restaurant.new("Restaurant #{i}")
+end
+
+Customer.all.each do |u|
+  puts u.first_name
+end
+
+Restaurant.all.each do |p|
+  puts "#{p.name}"
+end
+
+
+
+binding.pry
