3ba55919258780136b09e878919b5e6b5409efd0
github username <github email address>
2017-12-22 10:36:28 -0500

    Done.

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -1,8 +1,34 @@
 class Restaurant
   attr_accessor :name
 
+  @@all = []
+
   def initialize(name)
     @name = name
+    @@all << self
+  end
+
+  def self.all
+    @@all
+  end
+
+  def self.find_by_name(name)
+    self.all.find do |rest|
+      rest.name == name
+    end
+  end
+
+  def reviews
+    all_rev = Review.all.select do |rev|
+      rev.restaurant == self
+    end
+  end
+
+  def customers
+    all_cust = Review.all.select do |rev|
+      rev.restaurant == self
+    end
+    all.cust.map {|r| r.customer}
   end
 
 end
--- tools/console.rb
+++ tools/console.rb
@@ -5,3 +5,12 @@ def reload
 end
 
 Pry.start
+
+c1 = Customer.new("Lindsay", "Criswell")
+c2 = Customer.new("Ian", "Port")
+
+rest1 = Restaurant.new("Chez Panisse")
+rest2 = Restaurant.new("Chipotle")
+
+rev1 = Review.new("AMAZING", c1, rest1)
+rev2 = Review.new("Meh", c2, rest2)
